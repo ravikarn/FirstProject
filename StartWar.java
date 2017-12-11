567e630c39cd7f712f4a61b0aca99f0261010e4b
Ravi Shankar Karn <ravi.karn@tcs.com>
2017-12-11 18:53:37 +0530

    updated

--- StartWar.java
+++ StartWar.java
@@ -6,30 +6,95 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.Scanner;
 
-public class StartWar {
+class StartWar {
+	static Map<String,Integer> decodingMap=new HashMap<>();
+
+	static void startGame(int[][] playerA, int[][] playerB, int strengthOfPlayerA, int strengthOfPlayerB, Map<String, String> attackPlan) {
+		String[] playerAPlan=attackPlan.get("A").split(" ");
+		String[] playerBPlan=attackPlan.get("B").split(" ");
+
+		boolean trunOfA=true;
+
+		int playerACounter=0;
+		int playerBCounter=0;
+
+		while(strengthOfPlayerA>0 || strengthOfPlayerB>0) {
+			if(trunOfA) {
+				String attackPoint=playerAPlan[playerACounter];
+				String[] attackPointRowColumn=attackPoint.split("");
+				
+				int row=decodingMap.get(attackPointRowColumn[0]);
+				int column=(Integer.parseInt(attackPointRowColumn[1]))-1;
+				
+				int strikeValueForA=playerB[row][column];
+				
+				if(0==strikeValueForA) {
+					
+					trunOfA=false;
+				}else {
+					strengthOfPlayerB--;
+					trunOfA=true;
+				}
+				
+				
+
+				playerACounter++;
+			}else {
+				String attackPoint=playerBPlan[playerBCounter];
+				String[] attackPointRowColumn=attackPoint.split("");
+				
+				int row=decodingMap.get(attackPointRowColumn[0]);
+				int column=(Integer.parseInt(attackPointRowColumn[1]))-1;
+				
+				int strikeValueForB=playerA[row][column];
+				
+				if(0==strikeValueForB) {
+					
+					trunOfA=true;
+				}else {
+					strengthOfPlayerA--;
+					trunOfA=false;
+				}
+
+				playerBCounter++;
+			}
+
+
+
+
+		}
+
+
+
+		System.out.println("Start Game Called");
+	}
+
 	public static void main(String[] args) throws FileNotFoundException {
 
 		String filePath="input.txt";
 		File file = new File(filePath);
 		Scanner sc = new Scanner(file);
 		int input=0;
-		Map<String,Integer> decodingMap=new HashMap<>();
+		int totalNumberOfShips=0;
+
+
+		int strengthOfPlayerA=0;
+		int strengthOfPlayerB=0;
+
+
+
+		
 
 		for(int i=1;i<=26;i++) {
 			decodingMap.put(Character.toString((char)(i+64)), i);
 		}
-
-
-
-
 		int column=0;
 		int row=0;
-		int totalNumberOfShips=0;
-		int numberOfPShips=0;
-		int numberOfQShips=0;
+		int numberOfShips=0;
+		Map<String,String> attackPlan=new HashMap<>(); 
 
-		int[][] playerA=new int[row][];
-		int[][] playerB=new int[row][];
+		int[][] playerA = null;
+		int[][] playerB = null;
 
 		boolean flagOfA=true;
 		while(sc.hasNextLine()){
@@ -38,69 +103,68 @@ public class StartWar {
 				String[] inputArray=line.split(" ");
 				column=Integer.parseInt(inputArray[0]);
 				row=decodingMap.get(inputArray[1]);
-
-
-				playerA=new int[row][];
-				playerB=new int[row][];
+				playerA=new int[row][column];
+				playerB=new int[row][column];
 
 			}else if(1==input) {
 				totalNumberOfShips=Integer.parseInt(line);
-
 			}else{
-
-
 				String[] inputArray=line.split(" ");
-				int lengthOfInputArray=inputArray.length;
 
 				String typeOfShip=inputArray[0];
 
-				int columnDimensionOfShip=0;
-				int rowDimensionOfShip=0;
-
 				if("P".equalsIgnoreCase(typeOfShip) || "Q".equalsIgnoreCase(typeOfShip)) {
-					numberOfPShips=((lengthOfInputArray)-3)/2;
+					int lengthOfInputArray=inputArray.length;
+					int columnDimensionOfShip=0;
+					int rowDimensionOfShip=0;
+
+					int costOfShip=("P".equalsIgnoreCase(typeOfShip))?1:2;
+
+					numberOfShips=((lengthOfInputArray)-3)/2;
 					columnDimensionOfShip=Integer.parseInt(inputArray[1]);
 					rowDimensionOfShip=Integer.parseInt(inputArray[2]);
-					
-					
 					boolean shipB=false;
-					
+
 					for(int i=3;i<lengthOfInputArray;i++) {
 						String[] positionOfShip=inputArray[i].split("");
-						
+						int shipRowStart=(decodingMap.get(positionOfShip[0]))-1;
+						int shipColumnStart=(Integer.parseInt(positionOfShip[1]))-1;
 						if(shipB) {
-							System.out.println("B postion---"+positionOfShip[0]+""+positionOfShip[1]);
+							for(int j=shipRowStart;j<(shipRowStart+rowDimensionOfShip);j++) {
+								for(int k=shipColumnStart;k<(shipColumnStart+columnDimensionOfShip);k++) {
+									playerB[j][k]=costOfShip;
+									strengthOfPlayerB=strengthOfPlayerB+costOfShip;
+								}
+							}
 							shipB=false;
 						}else {
-							System.out.println("A postion---"+positionOfShip[0]+""+positionOfShip[1]);
+
+							for(int j=shipRowStart;j<(shipRowStart+rowDimensionOfShip);j++) {
+								for(int k=shipColumnStart;k<(shipColumnStart+columnDimensionOfShip);k++) {
+									playerA[j][k]=costOfShip;
+									strengthOfPlayerA=strengthOfPlayerA+costOfShip;
+								}
+							}
 							shipB=true;
 						}
 					}
-					
-					System.out.println("number of "+typeOfShip+" is -"+numberOfPShips+" having row "+rowDimensionOfShip+" and column "+columnDimensionOfShip);
-					
-
-
 				}else {
-
 					if(flagOfA) {
-						System.out.println("A attack plan- "+line);
+
+						attackPlan.put("A", line);
+
 						flagOfA=false;
 					}else {
-						System.out.println("B attack plan- "+line);
+						attackPlan.put("B", line);
 						flagOfA=true;
 					}
 				}
-
 			}
-
 			input++;
-
 		}
 
 
-
+		startGame(playerA,playerB,strengthOfPlayerA,strengthOfPlayerB,attackPlan);
 
 	}
-
 }
